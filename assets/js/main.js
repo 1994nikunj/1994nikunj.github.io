0920f28f8eac79cf8b432a5c2584c1bc2222ec5f
Nikunj Sharma <1994nikunj@gmail.com>
2023-03-26 23:20:56 -0400

    - major visual changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// CODE BLOCKS
 jQuery(document).ready(function ($) {
     $('.level-bar-inner').css('width', '0');
     $(window).on('load', function () {
@@ -11,3 +12,29 @@ jQuery(document).ready(function ($) {
         });
     });
 });
+
+// SHOW/HIDE CODE BLOCKS
+var showButtons = document.getElementsByClassName("show-button");
+
+// Update the text content of all buttons
+for (var i = 0; i < showButtons.length; i++) {
+    var showButton = showButtons[i];
+    var targetID = showButton.getAttribute("data-target");
+    var targetSection = document.getElementById(targetID);
+    var sectionName = showButton.textContent;
+
+    showButton.textContent = sectionName;
+    showButton.addEventListener("click", createToggleFunction(showButton, targetSection, sectionName));
+}
+
+function createToggleFunction(button, section, sectionName) {
+    return function () {
+        if (section.classList.contains("hidden")) {
+            section.classList.remove("hidden");
+            button.textContent = sectionName;
+        } else {
+            section.classList.add("hidden");
+            button.textContent = sectionName;
+        }
+    };
+}
